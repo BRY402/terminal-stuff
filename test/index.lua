f0b686e4d149c2bbb31d2b9358db698c1e8f9400
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-07-01 03:36:45 -0300

    Update test/index.lua

--- test/index.lua
+++ test/index.lua
@@ -1,3 +1,22 @@
 -- index.lua
 display("yeah babyyyy")
+local function load(filelocation)
+    local split = string.split(url, "/")
+    table.remove(split, #split)
+    local splitlocation = string.split(filelocation, "/")
+    for i = 1, #splitlocation do
+        local v = splitlocation[#splitlocation]
+        if v == ".." then
+            table.remove(split, #split)
+            table.remove(splitlocation, i)
+        elseif v == "." then
+            table.remove(splitlocation, i)
+        end
+    end
+    local url = table.concat(split, "/").."/"..filelocation
+    local splitextension = string.split(url, ".")
+    local extension = extensions[splitextension[#splitextension]]
+    assert(extension, "Invalid extension or none was provided.")
+    return extension(HttpService:GetAsync(url))
+end
 load("test.lua")
