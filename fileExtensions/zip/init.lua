f76dee6cd4c38b443edf77e04a923a0edfd04920
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-09-25 18:18:39 -0300

    Update fileExtensions/zip/init.lua

--- fileExtensions/zip/init.lua
+++ fileExtensions/zip/init.lua
@@ -3,5 +3,7 @@ terminal.extensions.zip = function(file)
     return DataSerializer.Deserialize(file.Source)
 end
 file.Events.FileCreated:Connect(function(file)
-    if string.match(file.Name or "", ".*%.zip")
+    if string.match(file.Name or "", ".*%.zip") then
+        file.Source = DataSerializer.Serialize({file.Source})
+    end
 end)
